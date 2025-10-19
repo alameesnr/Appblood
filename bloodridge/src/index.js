2d6739b49fee2e01e36902e1ff8b393e9266c3bc
alameesnr <145246885+alameesnr@users.noreply.github.com>
2025-10-19 13:57:55 +0100

    try

--- bloodridge/src/index.js
+++ bloodridge/src/index.js
@@ -17,5 +17,5 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen()
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
